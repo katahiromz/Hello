7c26c538ed600ee319f5a24438942a2bdc2bac59
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-04-09 15:39:48 +0900

    update hello12

--- hello12/hello12.cpp
+++ hello12/hello12.cpp
@@ -6,6 +6,9 @@ int main(void)
 {
     HIMC hIMC;
     BOOL ret;
+    printf("ImmCreateContext: %p\n", ImmCreateContext);
+    printf("ImmDestroyContext: %p\n", ImmDestroyContext);
+    fflush(stdout);
     DebugBreak();
     hIMC = ImmCreateContext();
     printf("hIMC: %p\n", hIMC);
